0783e4267bbbae2af71a963e92f0589012ce2975
yutinggh <134508301+yutinggh@users.noreply.github.com>
2023-07-19 11:23:44 +0800

    upload images for home page

---
+++ frontend/src/resources/img/ArrowAngleLeft.tsx
@@ -0,0 +1,7 @@
+const ArrowAngleLeft = () => (
+  <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" clipRule="evenodd" d="M10.8283 13.1623C10.5453 13.4049 10.1192 13.3721 9.87654 13.0891L6.80002 9.49982L9.87654 5.91053C10.1192 5.62749 10.5453 5.59471 10.8283 5.83732C11.1114 6.07993 11.1441 6.50606 10.9015 6.7891L8.57807 9.49982L10.9015 12.2105C11.1441 12.4936 11.1114 12.9197 10.8283 13.1623Z" fill="black" fillOpacity="0.91"/>
+  </svg>
+);
+
+export default ArrowAngleLeft;
