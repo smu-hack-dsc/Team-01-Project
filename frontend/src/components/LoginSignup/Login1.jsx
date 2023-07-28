53c528f569aedc93306650f1d1c8d3704ae2318d
swiifttay <126874041+swiifttay@users.noreply.github.com>
2023-07-29 02:38:13 +0800

    toggle switch edit

--- frontend/src/components/LoginSignup/Login1.jsx
+++ frontend/src/components/LoginSignup/Login1.jsx
@@ -9,10 +9,10 @@ const Login1 = () => {
       <div class="absolute left-0 w-screen h-screen flex flex-col justify-start items-center pt-5 overflow-hidden">
         <div class="absolute w-[200vw] h-full -rotate-12 -top-2/3 -left-1/3 bg-gray-200 z-1" />
         <div class="flex relative top-[120px] flex-col justify-center items-center z-2">
-          <div class="flex text-black text-center font-sans text-2xl font-bold pb-12 pt-12">
+          <div class="flex text-black text-center font-RecoletaAlt text-2xl font-bold pb-12 pt-12">
             Choose an account
           </div>
-          <div class="pb-16 pt-4 items-center">
+          <div class="pb-16 pt-4 items-center justify-center w-5/6">
             <ToggleSwitch />
           </div>
           <LoginInput />
--- frontend/src/components/LoginSignup/ToggleSwitch.jsx
+++ frontend/src/components/LoginSignup/ToggleSwitch.jsx
@@ -5,52 +5,29 @@ import React, { useState } from 'react';
 const ToggleSwitch = () => {
   const [isOption1Selected, setOption1Selected] = useState(true);
 
-  const switchStyles = css`
-    display: flex;
-    width: 400px;
-    height: 65px;
-    background-color: #D9D9D9;
-    border-radius: 50px;
-    cursor: pointer;
-    align-items: center;
-  `;
-
-  const optionStyles = css`
-    flex: 1;
-    text-align: center;
-    padding: 30px 8px;
-    user-select: none;
-    font-family: Uncut Sans;
-    font-size: 24px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 16px; /* 66.668% */
-    letter-spacing: -0.12px;
-  `;
-
-  const option1Styles = css`
-    background-color: ${isOption1Selected ? '#9663FC' : '#D9D9D9'};
-    color: ${isOption1Selected ? '#FFF' : '#000'};
-    border-top-left-radius: 50px;
-    border-bottom-left-radius: 50px;
-  `;
-
-  const option2Styles = css`
-    background-color: ${isOption1Selected ? '#D9D9D9' : '#9663FC'};
-    color: ${isOption1Selected ? '#000' : '#FFF'};
-    border-top-right-radius: 50px;
-    border-bottom-right-radius: 50px;
-  `;
-
   const handleToggle = () => {
     setOption1Selected(!isOption1Selected);
   };
 
+  // might want to consider making a moving flex at the back to allow for transition animation
   return (
-    <div css={switchStyles} onClick={handleToggle}>
-      <div css={[optionStyles, option1Styles]}>Volunteer</div>
-      <div css={[optionStyles, option2Styles]}>Organisation</div>
+    <div class="flex flex-row justify-center items-center">
+      <div class="w-full h-20 bg-gray-400 cursor-pointer items-center rounded-3xl" onClick={handleToggle}>
+         {isOption1Selected ? (
+            <div class="flex flex-row">
+              <div class="flex w-3/6 h-20 items-center justify-center text-center font-RecoletaAlt text-2xl bg-purple-500 text-white rounded-l-3xl">Volunteer</div>
+              <div class="flex w-3/6 h-20 items-center justify-center text-center font-RecoletaAlt text-2xl ">Organisation</div>
+            </div>
+        ) : (
+          <div class="flex flex-row">
+            <div class="flex w-3/6 h-20 items-center justify-center text-center font-RecoletaAlt text-2xl">Volunteer</div>
+            <div class="flex w-3/6 h-20 items-center justify-center text-center font-RecoletaAlt text-2xl bg-purple-500 text-white rounded-r-3xl">Organisation</div>
+          </div>
+        )}
+      
+      </div>
     </div>
+
   );
 };
 
